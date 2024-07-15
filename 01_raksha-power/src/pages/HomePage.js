51aff8f17f5f85fb86be419f00a5877b92051f88
ArzoRaza <ccoding76@gmail.com>
2024-07-15 23:15:59 +0530

    new initialized Project

---
+++ 01_raksha-power/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Raksha Power Insurance</h1>
+      <p>Your trusted partner in insurance.</p>
+    </div>
+  );
+}
+
+export default HomePage;
